fd316db7ba74e1e5f556c71b98c0bfb41d3e468b
takumi_yamamoto <takumi_yamamoto@gdo.co.jp>
2022-09-19 14:39:07 +0900

    lintの設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
-import Head from "next/head"
-import { createGlobalStyle } from "styled-components"
+import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   html,
@@ -31,7 +31,11 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <meta key="charset" name="charset" content="utf-8" />
-        <meta key="viewport" name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5" />
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+        />
         <meta property="og:local" content="ja_JP" />
         <meta property="og:type" content="website" />
       </Head>
