e0774c1f6232f9de9cc1b7fd22f508ecd42fd8d4
takumi_yamamoto <takumi_yamamoto@gdo.co.jp>
2022-09-19 15:36:27 +0900

    storybookの環境設定

---
+++ src/themes/index.ts
@@ -0,0 +1,13 @@
+import colors from "./colors"
+import fontSizes from "./fontSizes"
+import latterSpacings from "./latterSpacings"
+import lineHeights from "./lineHeights"
+import space from "./space"
+
+export const theme = {
+  space,
+  fontSizes,
+  latterSpacings,
+  lineHeights,
+  colors
+} as const
