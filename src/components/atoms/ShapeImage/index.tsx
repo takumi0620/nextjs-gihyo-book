99e7dfac0282f9cccc1a68c8fa44474ff39c6419
takumi0620 <grassy0620aqua@gmail.com>
2022-09-24 15:16:19 +0900

    ShapeImage実装

---
+++ src/components/atoms/ShapeImage/index.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+import Image, { ImageProps } from "next/image"
+
+type ImageShape = "circle" | "square"
+type ShapeImageProps = ImageProps & { shape?: ImageShape }
+
+const ImageWithShape = styled(Image)<{ shape?: ImageShape }>`
+  border-radius: ${({ shape }) => (shape === "circle" ? "50%" : "0")};
+`
+
+const ShapeImage = (props: ShapeImageProps) => {
+  const { shape, ...imageProps } = props
+
+  return <ImageWithShape shape={shape} {...imageProps} />
+}
+
+export default ShapeImage
